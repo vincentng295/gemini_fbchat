17539cdd5946d0cd159fab78d51110c28e9aad59
vincentng295 <151985750+vincentng295@users.noreply.github.com>
2025-04-09 23:44:22 +0700

    Update css for getting Facebook profile link

--- aichat.py
+++ aichat.py
@@ -443,7 +443,7 @@ try:
                             print_with_time(e)
                         
                         try:
-                            profile_btn = driver.find_elements(By.CSS_SELECTOR, 'a.x1i10hfl.x1qjc9v5.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x1q0g3np.x87ps6o.x1lku1pv.x1rg5ohu.x1a2a7pz.xs83m0k')
+                            profile_btn = driver.find_elements(By.XPATH, "//a[.//h2]")
                             facebook_info = None
                             facebook_id = None
                             if len(profile_btn) > 0:
