d109a7f1119ca2a84e866a032d832c8738b18905
vincentng295 <151985750+vincentng295@users.noreply.github.com>
2025-04-09 11:56:52 +0700

    No need to test bot anymore

--- test_selenium.py
+++ test_selenium.py
@@ -13,20 +13,7 @@ try:
         (scoped_dir, False, True),
     ]:
         driver = __chrome_driver__(*opt)
-        driver.get("https://deviceandbrowserinfo.com/are_you_a_bot")
         drivers.append(driver)
-    time.sleep(5)
-    for driver in drivers:
-        wait = WebDriverWait(driver, 20)
-        wait.until(
-            lambda d: d.execute_script("return document.readyState") == "complete"
-        )
-        resultsBotTest = driver.find_element(By.ID, "resultsBotTest").text
-        resultsBotTestDetails = driver.find_element(By.ID, "resultsBotTestDetails").text
-        print("resultsBotTest:", resultsBotTest)
-        print("resultsBotTestDetails:", resultsBotTestDetails)
-        driver.quit()
-    
 except Exception as e:
     print(e)
 finally:
